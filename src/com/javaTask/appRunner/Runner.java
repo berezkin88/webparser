52b20fa434a280f4873cf0a6a787f720b06ad047
Alexander <berezkin05@gmail.com>
2019-05-05 14:05:39 +0300

    make amendments, create service layer, add tests for service and utils

--- src/com/javaTask/appRunner/Runner.java
+++ src/com/javaTask/appRunner/Runner.java
@@ -1,64 +1,21 @@
 package com.javaTask.appRunner;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Logger;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javaTask.entity.Website;
 import com.javaTask.exceptions.IOExc;
 import com.javaTask.exceptions.JAXBExc;
-import com.javaTask.utils.Utilities;
+import com.javaTask.service.AppService;
 
 public class Runner {
-	private static Logger log = Logger.getLogger(Website.class.getName());
-	static ObjectMapper mapper = new ObjectMapper();
 	
-	public static void main(String[] args) throws IOException {
-		Website web = collectData("https://prom.ua/p895411456-igrovoj-noutbu-omen.html");
-		
-		log.info("Started writing to JSON");
-		mapper.writeValue(new File(
-			System.getProperty("user.dir") + File.separator + "data" + File.separator + "website.json"), web);
-		log.info("Finished writing to JSON");
-		
-		try {
-			writeToXML(web);
-		} catch (JAXBExc e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static void writeToXML(Website web) throws JAXBExc {		
-		try {
-			log.info("Started writing to XML");
-			JAXBContext context = JAXBContext.newInstance(Website.class);
-			Marshaller marshaller = context.createMarshaller();
-			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-			marshaller.marshal(web, new File(System.getProperty("user.dir") + File.separator + "data" + File.separator + "website.xml"));
-			log.info("Finished writing to XML");
-		} catch (JAXBException e) {
-			throw new JAXBExc("Exception is thrown in writeToXML() method");
-		}
-	}
+	public static void main(String[] args) {
+		Website web = AppService.collectData("https://prom.ua/p895411456-igrovoj-noutbu-omen.html");
 
-	public static Website collectData(String url) {
-		log.info("Started collecting web data");
-		Website website = new Website(url);
-		
 		try {
-			Utilities.setMainInfo(website);
-			Utilities.setSpecs(website);
-			Utilities.setDesc(website);
-		} catch (IOExc e) {
+			AppService.writeToJSON(web);
+			AppService.writeToXML(web);
+		} catch (IOExc | JAXBExc e) {
 			e.printStackTrace();
 		}
-		
-		log.info("Finished collecting web data");
-		return website;
+
 	}
 }
--- src/com/javaTask/utils/Utilities.java
+++ src/com/javaTask/utils/Utilities.java
@@ -14,20 +14,15 @@ import com.javaTask.exceptions.IOExc;
 public class Utilities {
 	private static Document doc;
 	
-	public static void setMainInfo(Website web) throws IOExc {
+	public static void setImageUrl(Website web) throws IOExc {
 		try {
 			doc = Jsoup.connect(web.getUrl()).get();
-			
 		} catch (IOException e) {
 			throw new IOExc("Exception thrown in setMainInfo() method");
 		}
 		
-		Elements info = doc.select(".x-product-page__main-info");
-		web.setName(info.select(".x-title").first().html());
-		web.setPrice(Double.parseDouble(info.select(".x-product-price__value").first().attr("data-qaprice")));
-		
 		//Sequence of images
-		String images = info.select("[data-bazooka-props]").attr("data-bazooka-props");
+		String images = doc.getElementsByAttribute("data-bazooka-props").attr("data-bazooka-props");
 		
 		//getting necessary image among all
 		web.setImageUrl(images.substring(images.indexOf("image_url_640x640\": \"") + "image_url_640x640\": \"".length(), images.indexOf("\", \"id")));
@@ -42,9 +37,8 @@ public class Utilities {
 			throw new IOExc("Exception thrown in setSpecs() method");
 		}
 
-		Elements specs = doc.select(".x-product-attr");
-		Elements keys = specs.select("[data-qaid=\"attribute_line\"] > .x-attributes__left > .x-attributes__value");
-		Elements values = specs.select("[data-qaid=\"attribute_line\"] > .x-attributes__right > .x-attributes__value");
+		Elements keys = doc.select("[data-qaid=\"attribute_line\"] > .x-attributes__left > .x-attributes__value");
+		Elements values = doc.select("[data-qaid=\"attribute_line\"] > .x-attributes__right > .x-attributes__value");
 		
 		for(int i = 0, size = keys.size(); i < size; i++) {
 			transSpecs.put(keys.get(i).text(), values.get(i).text());
@@ -60,6 +54,26 @@ public class Utilities {
 			throw new IOExc("Exception thrown in setDesc() method");
 		}
 		
-		web.setDesc(doc.select(".x-user-content").text());
+		web.setDesc(doc.getElementsByAttributeValue("data-qaid", "product_description").text());
+	}
+	
+	public static void setName(Website web) throws IOExc {
+		try {
+			doc = Jsoup.connect(web.getUrl()).get();
+		} catch (IOException e) {
+			throw new IOExc("Exception thrown in setMainInfo() method");
+		}
+		
+		web.setName(doc.getElementsByAttributeValue("data-qaid", "product_name").text());
+	}
+	
+	public static void setPrice(Website web) throws IOExc {
+		try {
+			doc = Jsoup.connect(web.getUrl()).get();
+		} catch (IOException e) {
+			throw new IOExc("Exception thrown in setMainInfo() method");
+		}
+		
+		web.setPrice(Double.parseDouble(doc.getElementsByAttribute("data-qaprice").attr("data-qaprice")));
 	}
 }
